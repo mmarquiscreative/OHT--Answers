d561e624e02b86b4095a1041c149aa1282e99d69
mmarquiscreative <mmarquis.creative@gmail.com>
2017-08-09 16:57:10 -0700

    Tidying up
    • Began adding clarity comments
    • Simplifying long functions into a series of smaller functions.

--- app.js
+++ app.js
@@ -28,37 +28,36 @@ var speechQuiz={
 
 function loadRandomOrder(){
     
-    // 1. Calculate 9 groups of 3 numbers
+    // Calculate 9 groups of 3 numbers
     for (i = 0; i < 9; i++){
         var num1, num2, num3, answer1, answer2, answer3;
         
-        // 1.1 Calculate number in group of 3
+        // 1. Calculate number in group of 3
         num1 = Math.round(Math.random()*8);
         num2 = Math.round(Math.random()*8);
         num3 = Math.round(Math.random()*8);
         
-        // 1.2 prevent duplicates in groups
+        // 2. prevent duplicates in groups
         while (num1 === num2 || num2 === num3 || num1 === num3){
             num2 = Math.round(Math.random()*8);
             num3 = Math.round(Math.random()*8);
         }
         
+        // 3. update which answers match numbers generated
         answer1 = speechQuiz.questions[num1];
         answer2 = speechQuiz.questions[num2];
         answer3 = speechQuiz.questions[num3];
         
+        // 4. add three new answers to end of order array
         speechQuiz.questionsOrder.push(answer1);
         speechQuiz.questionsOrder.push(answer2);
         speechQuiz.questionsOrder.push(answer3);
         
     }
-console.log(speechQuiz.questionsOrder);
-    console.log(speechQuiz.questions);
-    // 2. Push numbers into questions array
 }
 
 
-
+// Answer Object Constructor
 function Answer(question, id, parentId, volume){
     this.question = question;
     this.id=id;
@@ -66,12 +65,14 @@ function Answer(question, id, parentId, volume){
     this.isCorrect = (question === id || question === parentId);
 }
 
+// Calculate results of questions, add to results object
 function howWell(){
     var right, wrong, percentageRight, percentageWrong, num;
     
     right = 0;
     wrong = 0;
         
+    // 1. update right and wrong totals for each answer
     speechQuiz.answers.forEach(function(cur){
         
         if (cur.isCorrect) {
@@ -81,53 +82,58 @@ function howWell(){
         } else {
             console.log('answers for each did not work here ' + cur);
         }
-    })
-    console.log(right);
-    console.log(wrong);
+    });
+    
+    // 2. Establish the total number of answers
     num = speechQuiz.answers.length;
-        
-        percentageRight = Math.round((right / num) * 100);
-        
-        percentageWrong = Math.round((wrong / num) * 100);
-        
-     
-            testResults.speech.totalRight = right,
-            testResults.speech.totalWrong = wrong,
-            testResults.speech.percentRight = percentageRight,
-            testResults.speech.percentWrong = percentageWrong
+    
+    // 3. Calculate percent right and wrong
+    percentageRight = Math.round((right / num) * 100);
+    percentageWrong = Math.round((wrong / num) * 100);
+
+    // 4. Add results to result obj
+    testResults.speech.totalRight = right;
+    testResults.speech.totalWrong = wrong;
+    testResults.speech.percentRight = percentageRight;
+    testResults.speech.percentWrong = percentageWrong;
         
 }
 
-function init(){
-    // 1. load speech test HTML
+// 2. Update html to match answers provided as arguments
+function updateAnsHTML(){
     for (i = 0; i < 9; i++){
         cur = ans[i];
         tempNum = i + 1;
         
-    document.querySelector('#ans' + tempNum).id = cur;
-    document.querySelector('#' + cur).innerHTML = '<p>' + cur + '</p>';
-    }
-   
-    // 2. add event listener
-    document.querySelector('.container-fluid').addEventListener('click', function(){
-    
-        // 1. add new answer obj
-        speechQuiz.answers.push(new Answer(speechQuiz.questionsOrder[curRound], event.target.id, event.target.parentNode.id, Math.random()));
+        // 2.1 Update HTML ids for answer divs
+        document.querySelector('#ans' + tempNum).id = cur;
         
-        // 2. Log target
+        // 2.2 Add answer text inside answer divs
+        document.querySelector('#' + cur).innerHTML = '<p>' + cur + '</p>';
+    }
+}
+
+function addNewAnswer(){
+// 1. add new answer obj
+        speechQuiz.answers.push(new Answer(speechQuiz.questionsOrder[curRound], event.target.id, event.target.parentNode.id, Math.random()));
+};
+        
+function logTarget(){// 2. Log target
         console.log(curRound);
         console.log(speechQuiz.questionsOrder[curRound]);
         console.log(event.target.id);
         console.log(speechQuiz.answers[curRound].isCorrect);
         answerCounter++;
         curRound++;
-        
-        // 4. Update Answer Progress
+};
+
+function updateRoundProg(){        // 4. Update Answer Progress
         if (answerCounter < 4){
             document.querySelector('#box' + (answerCounter)).classList.add('filled');
         }
-        
-        // 3. nextQuestion
+};
+       
+function loadNextQuestion(){        // 3. nextQuestion
         if (answerCounter == 3){
         setTimeout(function(){
 askQuestion();
@@ -140,17 +146,49 @@ askQuestion();
             }
         }, 600)
         }
+};
+  
+function answerInput(){
+    
+    // 1. Add new answer obj
+    addNewAnswer();
+    
+    // 2. Log Target
+    logTarget();
+    
+    // 3. update progress meter
+    updateRoundProg();
+    
+    // 4. Load next question
+    loadNextQuestion();
+} 
         
-        
-        
-        
-});
+function init(){
+    // 1. load speech test HTML
+    
+    // 2. Update html to match answers provided as arguments
+   updateAnsHTML();
+   
+    // 2. add event listeners
+    document.querySelector('.container-fluid').addEventListener('click', answerInput);
 } 
 
 //
 function askQuestion(){
+   
+    /*
+    var conversation = 'Audio/BackgroundNoise.mp3';
+    var backgroundAud = new Audio(conversation);
+    backgroundAud.play();
+    backgroundAud.onended = function(){
+        backgroundAud.play();
+    }
+    backgroundAud.volume = (roundNum * .2 + .2);
+    
+    } else */
     // next question
-    if (roundNum <= 3){
+    if (roundNum < 4){
+        console.log('round num is ' + roundNum);
     var tempNum = curRound;
     // audio pathway string
     var curAudio1 = 'Audio/Speech_' + speechQuiz.questionsOrder[tempNum] + '.mp3';
@@ -160,10 +198,16 @@ function askQuestion(){
     var curAudio3 = 'Audio/Speech_' + speechQuiz.questionsOrder[tempNum] + '.mp3';
     
     // load path string as new Audio object
-    var audio1 = new Audio(curAudio1);
-    var audio2 = new Audio(curAudio2);
-    var audio3 = new Audio(curAudio3);
     
+        
+    var audio1 = new Audio(curAudio1);
+        audio1.volume = (1 - roundNum * .05);
+    var audio2 = new Audio(curAudio2);
+        audio2.volume = (1 - roundNum * .05);
+    var audio3 = new Audio(curAudio3);
+        audio3.volume = (1 - roundNum * .05);
+        
+   
     // play the 3 audio clips
     audio1.play();
     
@@ -178,7 +222,13 @@ function askQuestion(){
     // update total question rounds
     roundNum++;
 
+    }
     
+    if (roundNum > 3){
+        console.log('trying to pause')
+        backgroundAud.loop = false;
+        backgroundAud.pause();
+        backgroundAud.currentTime = 0;
     }
 }
 loadRandomOrder();
